08f6c164f2a94db0e9cb9630fbed5ed718d7fcb6
bsnk-dev <admin@bsnk.dev>
2021-07-26 20:30:48 -0500

    changed about size

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
   <div>
     <h1 class="mt-4">About</h1>
 
-    <div class="card text-center mt-4">
+    <div class="card text-center mt-4 ms-auto me-auto" style="max-width: 700px">
       <div class="card-header">
         About this program
       </div>
@@ -10,6 +10,16 @@
         <img src="@/assets/logo.png" height="48px" width="48px"/>
         <h5 class="card-title">Boltz Engine</h5>
         <p class="card-text">Free and open source. By bsnk-dev.</p>
+        <div>
+          <a class="btn btn-dark" href="https://github.com/bsnk-dev/boltz-engine" target="_blank">
+            <i class="bi bi-github"></i>
+            <span class="d-none ms-2 d-lg-inline-block">Engine</span>
+          </a>
+          <a class="btn btn-dark ms-2" href="https://github.com/bsnk-dev/boltz-web" target="_blank">
+            <i class="bi bi-github"></i>
+            <span class="d-none ms-2 d-lg-inline-block">Interface</span>
+          </a>
+        </div>
       </div>
       <div class="card-footer text-muted">
         &copy; Copyright {{ new Date().getFullYear() }} bsnk-dev
